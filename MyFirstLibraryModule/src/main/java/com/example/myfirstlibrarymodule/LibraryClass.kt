ff7a614de4bb7e0843373210ef43f66af35de16f
amitkumar38 <amit.kumar38@nagarro.com>
2023-09-12 13:48:39 +0530

    Initial commit

---
+++ MyFirstLibraryModule/src/main/java/com/example/myfirstlibrarymodule/LibraryClass.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstlibrarymodule
+
+import android.content.Context
+import android.widget.Toast
+
+class LibraryClass {
+    companion object{
+        fun showToast(context:Context){
+            Toast.makeText(context,"My First Toast",Toast.LENGTH_LONG).show()
+        }
+    }
+}
